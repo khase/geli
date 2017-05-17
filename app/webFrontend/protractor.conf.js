42e70886902c21ccc8f3674974d7ef551715dc37
Alexander Eimer <alexander.eimer@gmail.com>
2017-05-17 12:17:41 +0200

    Setup Protractor-Screenshot-Plugin

--- app/webFrontend/protractor.conf.js
+++ app/webFrontend/protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {SpecReporter} = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -17,14 +17,26 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {
+    }
   },
-  beforeLaunch: function() {
+  plugins: [{
+    package: 'protractor-screenshoter-plugin',
+    screenshotPath: './.reports/e2e',
+    screenshotOnExpect: 'failure+success',
+    screenshotOnSpec: 'failure+success',
+    imageToAscii: 'failure',
+    clearFoldersBeforeTest: true,
+    withLogs: false, // We could set this to true => https://www.npmjs.com/package/protractor-screenshoter-plugin#withlogs-chrome-only
+    writeReportFreq: 'asap',
+    verbose: 'debug'
+  }],
+  beforeLaunch: function () {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
   },
   onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
   }
 };
