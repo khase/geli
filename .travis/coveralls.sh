2fe27564feae0fe4f3723f7390d2db27c22c727c
Ken Hasenbank <Ken.Hasenbank@stud.h-da.de>
2017-10-27 23:15:59 +0200

    Update coveralls.sh

--- .travis/coveralls.sh
+++ .travis/coveralls.sh
@@ -30,7 +30,8 @@ fi
 cd ..
 
 echo "+ sending lcov file to coveralls"
-sed -i 's/bin\///g' api/coverage/lcov.info && cat api/coverage/lcov.info | $MODULE_PATH/$BIN_PATH/coveralls
+# sed -i 's/bin\///g' api/coverage/lcov.info && 
+cat api/coverage/lcov.info | $MODULE_PATH/$BIN_PATH/coveralls -v
 # cat api/coverage/lcov.info | $MODULE_PATH/$BIN_PATH/coveralls.js -v
 
 echo "+ INFO: Currently only the api-coverdata are generated and send"
