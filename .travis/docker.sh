3b25f18e261c26e2ca03d3a4679ffd90d43d79d8
Ken Hasenbank <Ken.Hasenbank@stud.h-da.de>
2017-12-05 14:19:46 +0100

    Update docker.sh

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -8,8 +8,8 @@ IPWD="$(pwd)"
 . ${DIR}/_shared-vars.sh
 
 function tag_and_push_api_and_web {
-    docker tag hdafbi/geli-api hdafbi/geli-api:${1}
-    docker tag hdafbi/geli-web-frontend hdafbi/geli-web-frontend:${1}
+    docker tag hdafbi/geli-api:latest hdafbi/geli-api:${1}
+    docker tag hdafbi/geli-web-frontend:latest hdafbi/geli-web-frontend:${1}
     docker push hdafbi/geli-api:${1}
     docker push hdafbi/geli-web-frontend:${1}
 }
